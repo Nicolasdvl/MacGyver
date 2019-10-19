56d45112a7e365d8abd9793d40ed40a7cc407c5e
Daval Nicolas <nicolasdvl@vivaldi.net>
2019-10-19 08:09:04 +0200

    fix setup ; add hero

---
+++ hero.py
@@ -0,0 +1,29 @@
+class hero:
+    def __init__(self):
+        self.pos = 16
+    
+
+    def movement(pos):
+        
+        print("Quel déplacement voulez-vous effectuer ?")
+        move = input()
+        if move == "z":
+            if level[pos - 15] == "x":
+                print("Un mur vous fait face")
+            elif level[pos - 15] == "p":
+                pos = pos - 15
+        if move == "s":
+            if level[pos + 15] == "x":
+                print("Un mur vous fait face")
+            elif level[pos + 15] == "p":
+                pos = pos + 15
+        if move == "q":
+            if level[pos - 1] == "x":
+                print("Un mur vous fait face")
+            elif level[pos - 1] == "p":
+                pos = pos - 1
+        if move == "d":
+            if level[pos + 1] == "x":
+                print("Un mur vous fait face")
+            elif level[pos + 1] == "p":
+                pos = pos + 1
--- setup.py
+++ setup.py
@@ -5,26 +5,19 @@ class setup:
         self.window = (window_width, window_height)
         self.sprite_width = window_width/15
         self.sprite_height = window_height/15
-        self.position = {}
 
-    def loading_stage(filename):
+    def loading_stage(self, filename):
 
         with open(filename, "r") as stage:
-            k = 0  
+            k = 0 
+            self = {}
             for i in range(15):
                 line = stage.readline()
 
                 for j in range(15):
                 
                     lettre = line [j]
-                    setup.position[k] = lettre
+                    self[k] = lettre
                     
                     k += 1
-        return setup.position
-
-setup.__init__(setup)
-setup.loading_stage("level.txt")
-
-print (setup.window)
-for i in range(15*15):
-    print(setup.position[i])
+        return self
